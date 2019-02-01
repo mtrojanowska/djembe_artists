0cee8daf3af93ff42fedc95447925934343ac5d5
Marta Trojanowska <m.troyanovska@gmail.com>
2019-02-01 21:16:43 +0100

    less loops

--- app.rb
+++ app.rb
@@ -18,10 +18,9 @@ artist1.add_to_list(song2)
 list.add_to_artists(artist)
 list.add_to_artists(artist1)
 
-loop do
+
   loop do
-    loop do
-      loop do
+
         puts "1. Show all Artists\n2. Show an Artist\n3. Add an Artist\n4. Quit the program"
         input1 = gets.chomp.to_i
 
@@ -31,62 +30,63 @@ loop do
         elsif input1 == 2
           p 'What Artist you desire to see?'
           required_artist = gets.chomp
-          if required_artist == artist.name
-            list.output_list(artist)
-          else
-            p 'No such artist'
-            break
-        end
+           if
+              required_artist == artist.name
+              list.output_list(artist)
+              p 'What next?'
 
-          loop do
             loop do
-              puts "1. Edit an Artist\n2. Add a Song to an Artist\n3. Edit a Song\n4. Delete a Song\n5. Delete an Artist\n6. Back"
-              input2 = gets.chomp.to_i
-              if input2 == 1
-                p 'Edit an Artist'\
-                       'Change the Name:'
-                changed_name = gets.chomp
-                p 'Change the Date of Birth:'
-                changed_birthdate = gets.chomp
-                p 'Change the Country of Origin:'
-                changed_origin = gets.chomp
-                artist.update(name: changed_name, birthdate: changed_birthdate, origin: changed_origin)
-                list.output_list(artist)
-              elsif input2 == 2
-                p 'Song Title:'
-                song_title = gets.chomp
-                p 'Released:'
-                song_premiere = gets.chomp
-                song = Song.new(title: song_title, premiere: song_premiere)
-                artist.add_to_list(song)
-                list.output_list(artist)
-              elsif input2 == 3
-                p 'Choose the Song:'
-                required_song = gets.chomp.capitalize!
-                p 'Change the Title'
-                changed_title = gets.chomp.capitalize!
-                p 'Change the date of release:'
-                changed_premiere = gets.chomp
-                if required_song == song.title
-                  song.update(title: changed_title, premiere: changed_premiere)
-                end
-                list.output_list(artist)
-              elsif input2 == 4
-                p 'What song You want to delete?'
-                required_song = gets.chomp.capitalize!
-                artist.delete_song(required_song)
-                list.output_list(artist)
-              elsif input2 == 5
-                list.delete_artist(artist)
-                list.show_artists
-              else input2 == 6
-                   break
-          end
-            end
+                  puts "1. Edit an Artist\n2. Add a Song to an Artist\n3. Edit a Song\n4. Delete a Song\n5. Delete an Artist\n6. Back"
+                  input2 = gets.chomp.to_i
+                  if input2 == 1
+                    p 'Edit an Artist'\
+                           'Change the Name:'
+                    changed_name = gets.chomp
+                    p 'Change the Date of Birth:'
+                    changed_birthdate = gets.chomp
+                    p 'Change the Country of Origin:'
+                    changed_origin = gets.chomp
+                    artist.update(name: changed_name, birthdate: changed_birthdate, origin: changed_origin)
+                    list.output_list(artist)
+                  elsif input2 == 2
+                    p 'Song Title:'
+                    song_title = gets.chomp
+                    p 'Released:'
+                    song_premiere = gets.chomp
+                    song = Song.new(title: song_title, premiere: song_premiere)
+                    artist.add_to_list(song)
+                    list.output_list(artist)
+                  elsif input2 == 3
+                    p 'Choose the Song:'
+                    required_song = gets.chomp.capitalize!
+                    p 'Change the Title'
+                    changed_title = gets.chomp.capitalize!
+                    p 'Change the date of release:'
+                    changed_premiere = gets.chomp
+                    if required_song == song.title
+                      song.update(title: changed_title, premiere: changed_premiere)
+                    end
+                    list.output_list(artist)
+                  elsif input2 == 4
+                    p 'What song You want to delete?'
+                    required_song = gets.chomp.capitalize!
+                    artist.delete_song(required_song)
+                    list.output_list(artist)
+                  elsif input2 == 5
+                    list.delete_artist(artist)
+                    list.show_artists
+                  else input2 == 6
             break
-          end
+                 end
+              break
+            end
+
+          else
+             p 'No such artist. Try again'
+           end
 
         elsif input1 == 3
+          p "Add an Artist"
           p "Artist's Name:"
           name_input = gets.chomp
           p 'Date of birth:'
@@ -95,29 +95,33 @@ loop do
           origin_input = gets.chomp
           artist = Artist.new(name: name_input, birthdate: birthdate_input, origin: origin_input)
           list.add_to_artists(artist)
-          loop do
-            p 'Whant to add a song to an artist? Y/N'
-            input3 = gets.chomp.capitalize!
-                  if input3 == 'Y'
-                  puts 'Add a song title:'
-                  title = gets.chomp
-                  p 'Add date of release:'
-                  premiere = gets.chomp
-                  song = Song.new(title: title, premiere: premiere)
-                  artist.add_to_list(song)
-                  list.output_list(artist)
-                  else input3 == 'N'
-              break
-            end
-          end
+                loop do
+                  p 'Whant to add a song to an artist? Y/N'
+                  input3 = gets.chomp.capitalize!
+                        if input3 == 'Y'
+                        puts 'Add a song title:'
+                        title = gets.chomp
+                        p 'Add date of release:'
+                        premiere = gets.chomp
+                        song = Song.new(title: title, premiere: premiere)
+                        artist.add_to_list(song)
+                        list.output_list(artist)
+                        else input3 == 'N'
+                    break
+                  end
+                end
           p 'Artist has been included'
           list.show_artists
-        else input1 == 4
-             break
-            end
-      end
+
+
+
+          elsif input1 == 4
+            break
+          else
+            p "Wrong input try again"
+            input1 = gets.chomp.to_i
+          end
+
+
+
     end
-    break
-  end
-  break
-end
