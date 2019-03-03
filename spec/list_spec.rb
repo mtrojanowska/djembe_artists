5a73cb6f59fb74a451199e4f0078eeb8c76f8f3f
Marta Trojanowska <m.troyanovska@gmail.com>
2019-03-03 17:05:29 +0100

    final

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -40,31 +40,3 @@ RSpec.describe List do
     end
   end
 end
-
-
-  # describe "#show_artist" do
-  #   it "finds an artist on the list of artists" do
-  #     jajo = Artist.new(name: "Jajo", birthdate: "999", origin: "GV")
-  #     toto = Artist.new(name: "Toto", birthdate: "000", origin: "SL")
-  #     first_song = Song.new(title: "First song", premiere: "00")
-  #     second_song = Song.new(title: "1 song", premiere: "00")
-  #     jajo.add_to_artists_song_list(first_song)
-  #     jajo.add_to_artists_song_list(second_song)
-  #     list = List.new
-  #     list.add_to_artists(jajo)
-  #     list.add_to_artists(toto)
-  #     artist_to_show = jajo
-  #     expect(list.show_artist(jajo)).to eq(artist_to_show.songs_list)
-  #   end
-  # end
-
-  # describe "#show_artists" do
-  #    it "show all artists on the list" do
-  #      jajo = Artist.new(name: "Jajo", birthdate: "999", origin: "GV")
-  #      toto = Artist.new(name: "Toto", birthdate: "000", origin: "SL")
-  #      list = List.new
-  #      list.add_to_artists(jajo)
-  #      list.add_to_artists(toto)
-  #      expect(list.show_artists).to eq([jajo, toto])
-  #    end
-  #  end
